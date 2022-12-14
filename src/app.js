507b48ffdc642d0bdb925554598bcc4ed99d4c1b
HelenHMac <120410707+HelenHMac@users.noreply.github.com>
2022-12-13 20:29:54 -0500

    Updated image

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -27,14 +28,14 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = response.data.wind.speed;
     dateElement.innerHTML = formatDate(response.data.dt*1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ce8e825e9de2cecd077619df37fa3d4f";
-let city = "Paris"
+let city = "Los Angeles"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 
 axios.get(apiUrl).then(displayTemperature);
-
-//! 
