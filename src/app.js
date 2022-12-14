0a689d07331a2801924da7a8913af46ed363c712
HelenHMac <120410707+HelenHMac@users.noreply.github.com>
2022-12-13 20:11:55 -0500

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -13,26 +13,25 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-
 function displayTemperature(response) {
-    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round (response.data.main.temp);
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.name;
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
     let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
-    windElement.innerHTML = response.data.wind.speed;
     let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formatDate(response.data.dt*1000);
 
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.wind.speed;
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
 }
 
 let apiKey = "ce8e825e9de2cecd077619df37fa3d4f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
 
